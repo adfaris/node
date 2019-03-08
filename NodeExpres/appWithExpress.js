43bd823ab5e5164753e6787a857a986da73bb31f
A.D.Faris <adfaris@gmail.com>
2019-03-07 22:48:40 -0500

    morgan

--- NodeExpres/appWithExpress.js
+++ NodeExpres/appWithExpress.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const app=express() 
+const morgan = require('morgan')
+const app = express()
+app.use(morgan('combined'))
 
-app.get('/ping', (req,res)=> {
-    res.send('pong')
+app.get('/ping', (req, res) => {
+  res.send('pong')
 })
 app.listen(8080, 'localhost')
